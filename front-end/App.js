1afa134c9acd7b6772527eb610f272bd61c287bc
minman93 <matt_j_inman@hotmail.co.uk>
2023-02-20 12:30:43 +0000

    home page started

--- front-end/App.js
+++ front-end/App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
+import HomePage from './components/home-page'
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <HomePage/>
   );
 }
 
@@ -17,4 +16,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
