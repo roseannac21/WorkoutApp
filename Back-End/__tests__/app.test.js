eb6bad0f2867574953bdb9793a59b26c791df7cd
Roseanna Craggs <roseannacraggs21@gmail.com>
2023-02-21 14:15:21 +0000

    post user complete, without passpord logic implemented yet

--- Back-End/__tests__/app.test.js
+++ Back-End/__tests__/app.test.js
@@ -1,13 +1,13 @@
-const request = require('supertest');
-const { app } = require('../app');
-const mongoose = require('mongoose');
-const Exercise = require('../schemas/ExerciseSchema');
-const { User } = require('../schemas/UserSchema');
-const Categories = require('../schemas/CategoriesSchema');
-const { exercises } = require('../data/exercises-data');
-const { users } = require('../data/user-data');
-const { categories } = require('../data/categories-data');
-const database = require('../connection')
+const request = require("supertest");
+const { app } = require("../app");
+const mongoose = require("mongoose");
+const Exercise = require("../schemas/ExerciseSchema");
+const { User } = require("../schemas/UserSchema");
+const Categories = require("../schemas/CategoriesSchema");
+const { exercises } = require("../data/exercises-data");
+const { users } = require("../data/user-data");
+const { categories } = require("../data/categories-data");
+const database = require("../connection");
 
 // require('dotenv').config({
 //   path: `${__dirname}/.env`,
@@ -22,64 +22,88 @@ const database = require('../connection')
 
 beforeEach(async () => {
   await User.deleteMany();
-  await User.insertMany(users);
+  await User.collection.insertMany(users);
   await Exercise.deleteMany();
-  await Exercise.insertMany(exercises);
+  await Exercise.collection.insertMany(exercises);
   await Categories.deleteMany();
-  await Categories.insertMany(categories);
+  await Categories.collection.insertMany(categories);
 });
 
 afterAll(async () => {
   await database.close();
 });
 
-describe('GET /api/users', () => {
-  test('should return all users', () => {
+describe("GET /api/users", () => {
+  test("should return all users", () => {
     return request(app)
-      .get('/api/users')
+      .get("/api/users")
       .expect(200)
       .then(({ body: { users } }) => {
         users.forEach((user) => {
-          expect(user).toHaveProperty('username', expect.any(String));
-          expect(user).toHaveProperty('password', expect.any(String));
-          expect(user).toHaveProperty('_id', expect.any(String));
-          expect(user).toHaveProperty('avatar_url', expect.any(String));
-          expect(user).toHaveProperty('__v', expect.any(Number));
+          expect(user).toHaveProperty("username", expect.any(String));
+          expect(user).toHaveProperty("password", expect.any(String));
+          expect(user).toHaveProperty("_id", expect.any(String));
+          expect(user).toHaveProperty("avatar_url", expect.any(String));
         });
         expect(Array.isArray(users)).toBe(true);
       });
   });
+  test("error handling- 404", () => {
+    return request(app).get("/api/usersssss").expect(404);
+  });
 });
-describe('GET /api/exercises', () => {
-  test('should return exercises', () => {
+describe("GET /api/exercises", () => {
+  test("should return exercises", () => {
     return request(app)
-      .get('/api/exercises')
+      .get("/api/exercises")
       .expect(200)
       .then(({ body: { exercises } }) => {
         exercises.forEach((exercise) => {
-          expect(exercise).toHaveProperty('_id', expect.any(String));
-          expect(exercise).toHaveProperty('name', expect.any(String));
-          expect(exercise).toHaveProperty('type', expect.any(String));
-          expect(exercise).toHaveProperty('muscle', expect.any(String));
-          expect(exercise).toHaveProperty('equipment', expect.any(String));
-          expect(exercise).toHaveProperty('difficulty', expect.any(String));
-          expect(exercise).toHaveProperty('instructions', expect.any(String));
-          expect(exercise).toHaveProperty('__v', expect.any(Number));
+          expect(exercise).toHaveProperty("_id", expect.any(String));
+          expect(exercise).toHaveProperty("name", expect.any(String));
+          expect(exercise).toHaveProperty("type", expect.any(String));
+          expect(exercise).toHaveProperty("muscle", expect.any(String));
+          expect(exercise).toHaveProperty("equipment", expect.any(String));
+          expect(exercise).toHaveProperty("difficulty", expect.any(String));
+          expect(exercise).toHaveProperty("instructions", expect.any(String));
         });
         expect(Array.isArray(exercises)).toBe(true);
       });
   });
+  test("error handling- 404", () => {
+    return request(app).get("/api/exercisessss").expect(404);
+  });
 });
-describe('GET /api/categories', () => {
-  test('should return categories', () => {
+describe("GET /api/categories", () => {
+  test("should return categories", () => {
     return request(app)
-      .get('/api/categories')
+      .get("/api/categories")
       .expect(200)
       .then(({ body: { categories } }) => {
         categories.forEach((category) => {
-          expect(category).toHaveProperty('category', expect.any(String));
-          expect(category).toHaveProperty('_id', expect.any(String));
+          expect(category).toHaveProperty("category", expect.any(String));
+          expect(category).toHaveProperty("_id", expect.any(String));
         });
       });
+  });
+  test("error handling- 404", () => {
+    return request(app).get("/api/categoriessss").expect(404);
+  });
+});
+describe("post user /api/users", () => {
+  test("returns status 200", () => {
+    return request(app).post("/api/users").expect(201);
+  });
+  test("user object is added to users collection", () => {
+    return request(app)
+      .post("/api/users")
+      .expect(201)
+      .then(({ body: { user } }) => {
+        expect(user.acknowledged).toBe(true);
+        expect(user).toHaveProperty("insertedId", expect.any(String));
+      });
+  });
+  test("error handling- 404", () => {
+    return request(app).post("/api/usersss").expect(404);
   });
 });
--- Back-End/controller.js
+++ Back-End/controller.js
@@ -1,6 +1,6 @@
-const Exercise = require('./schemas/ExerciseSchema');
-const { User } = require('./schemas/UserSchema');
-const Categories = require('./schemas/CategoriesSchema');
+const Exercise = require("./schemas/ExerciseSchema");
+const { User } = require("./schemas/UserSchema");
+const Categories = require("./schemas/CategoriesSchema");
 
 const getUsers = (req, res, next) => {
   return User.find()
@@ -26,4 +26,17 @@ const getCategories = (req, res, next) => {
     .catch(next);
 };
 
-module.exports = { getUsers, getExercises, getCategories };
+const postUser = (req, res, next) => {
+  return User.collection
+    .insertOne({
+      username: "user2",
+      password: "helloWorld",
+      avatar_url: "....",
+    })
+    .then((result) => {
+      res.status(201).send({ user: result });
+    })
+    .catch(next);
+};
+
+module.exports = { getUsers, getExercises, getCategories, postUser };
