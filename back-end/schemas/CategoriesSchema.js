ecab8d811fba766c89d6cd89321d2910d41def3a
nehcobiy <118573656+nehcobiy@users.noreply.github.com>
2023-02-23 09:20:42 +0000

    Revert "fixed post user"

--- back-end/schemas/CategoriesSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const CategoriesSchema = new mongoose.Schema({
-  category: { type: String, required: true }
-});
-
-const Categories = mongoose.model("Category", CategoriesSchema, "categories");
-
-module.exports = Categories;
