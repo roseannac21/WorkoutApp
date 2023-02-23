993d19b5fa602edea41e241929d8cf33ef7e115a
Roseanna Craggs <roseannacraggs21@gmail.com>
2023-02-23 10:49:40 +0000

    working on creating databse

--- Back-End/data/dev-data/insertData.js
+++ Back-End/data/dev-data/insertData.js
@@ -4,8 +4,8 @@ const Exercise = require("../../schemas/ExerciseSchema");
 const User = require("../../schemas/UserSchema");
 const Categories = require("../../schemas/CategoriesSchema");
 const { exercises } = require("./exercises-data");
-
 const { categories } = require("./categories-data");
+
 require("dotenv").config({
   path: `${__dirname}/../.env`,
 });
